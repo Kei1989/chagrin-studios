539c3c491cc8c3e8f4a09f54b5540261e0f6714d
Kei1989 <csierp@gmail.com>
2025-04-17 11:37:53 -0400

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,5 @@
-export async function onRequest(context) {
-  const db = context.env.devlogtest; // Your D1 binding
-  const result = await db.prepare("SELECT * FROM your_table LIMIT 1").first();
+ieexport async function onRequest(context) {
+  const db = context.env.DB; // Your D1 binding
+  const result = await db.prepare("SELECT * FROM users LIMIT 1").first();
   return new Response(JSON.stringify(result));
 }
